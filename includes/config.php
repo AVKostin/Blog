8ef928a758e85aafc0e94c17e247e9ffbda0084a
Alexander Kostin <asurv2@gmail.com>
2022-12-12 19:54:49 +0200

    update

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+$config = array(
+  'title' => 'Тестовый блог',
+    'db' => array(
+        'server' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'name' => 'blog_db'
+    )
+);
