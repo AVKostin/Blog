9d28ab089e3272ec6e958c25157e28db81043b62
Alexander Kostin <asurv2@gmail.com>
2022-12-13 19:38:44 +0200

    update

--- includes/footer.php
+++ includes/footer.php
@@ -8,7 +8,7 @@
           <li><a href="/">Главная</a></li>
               <li><a href="/pages/about_me.php">Обо мне</a></li>
               <li><a href="<?php echo $config['github_url']?>"target="_blank">Я в GitHub</a></li>
-            <li><a href="#">Правообладателям</a></li>
+            <li><a href="/pages/copyright.php">Правообладателям</a></li>
           </ul>
         </nav>
       </div>
