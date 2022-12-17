c61f7d40829b3f55d5e00fb1922d1a6f47c715b8
Alexander Kostin <asurv2@gmail.com>
2022-12-17 21:40:54 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,15 +1,15 @@
 <footer id="footer">
-      <div class="container">
-        <div class="footer__logo">
-          <h1><?php echo $config['title']; ?></h1>
-        </div>
-        <nav class="footer__menu">
-          <ul>
-          <li><a href="/">Главная</a></li>
-              <li><a href="/pages/about_me.php">Обо мне</a></li>
-              <li><a href="<?php echo $config['github_url']?>"target="_blank">Я в GitHub</a></li>
-            <li><a href="/pages/copyright.php">Правообладателям</a></li>
-          </ul>
-        </nav>
-      </div>
-    </footer>
+  <div class="container">
+    <div class="footer__logo">
+      <h1><?php echo $config['title']; ?></h1>
+    </div>
+    <nav class="footer__menu">
+      <ul>
+      <li><a href="/">Главная</a></li>
+          <li><a href="/pages/about_me.php">Обо мне</a></li>
+          <li><a href="<?php echo $config['github_url']?>"target="_blank">Я в GitHub</a></li>
+        <li><a href="/pages/copyright.php">Правообладателям</a></li>
+      </ul>
+    </nav>
+  </div>
+</footer>
