c85e7d0a6dc957e19c0c64bb365a78990c1516f1
Alexander Kostin <asurv2@gmail.com>
2022-12-17 21:40:28 +0200

    Update form.php

--- form.php
+++ form.php
@@ -1,14 +1,10 @@
-<?php
+<?php include('includes/db.php'); ?>
 
-include('includes/db.php')
-?>
+  <form method="POST" action="/handle.php">
+    <input type="text" placeholder="Ваш логин" name="login">
+    <input type="text" placeholder="Ваш пароль" name="password">
+    <hr>
+    <input type="submit" value="Подтвердить">
+  </form>
 
-<form method="POST" action="/handle.php">
-  <input type="text" placeholder="Ваш логин" name="login">
-  <input type="text" placeholder="Ваш пароль" name="password">
-  <hr>
-  <input type="submit" value="Подтвердить">
-</form>
-<?php
-mysqli_close($connection);
-?>
+<?php mysqli_close($connection); ?>
