bc1b6795598058069397c84efa380f0c29d12e3c
Alexander Kostin <asurv2@gmail.com>
2023-05-14 02:36:20 +0300

    Update

--- article.php
+++ article.php
@@ -9,60 +9,58 @@
   <div id="wrapper">
 
     <?php include "includes/header.php"; ?>
-        <?php
-          $article = mysqli_query($connection, "SELECT * FROM `articles` WHERE `id` = ". (int) $_GET['id']);
-          if( mysqli_num_rows($article) <= 0)
-            {
-              ?>
-                <div id="content">
-                  <div class="container">
-                    <div class="row">
-                      <section class="content__left col-md-8">
-                        <div class="block">
-                          <h3>статья не найдена</h3>
-                          <div class="block__content">
-                          <img src="/media/images/<?php echo $art['image'];?>" style="max-width: 100%">
-                            <div class="full-text">
-                              Запрашиваемая статья не существует!
-                            </div>
-                          </div>
-                        </div>
-                      </section>
-                      <!-- Секция сайдбара -->
-                      <section class="content__right col-md-4">
-                        <?php include "includes/sidebar.php";
-                        ?>
-                      </section>
-                    </div>
+    <?php
+    $article = mysqli_query($connection, "SELECT * FROM `articles` WHERE `id` = " . (int) $_GET['id']);
+    if (mysqli_num_rows($article) <= 0) {
+    ?>
+      <div id="content">
+        <div class="container">
+          <div class="row">
+            <section class="content__left col-md-8">
+              <div class="block">
+                <h3>статья не найдена</h3>
+                <div class="block__content">
+                  <img src="/media/images/<?php echo $art['image']; ?>" style="max-width: 100%">
+                  <div class="full-text">
+                    Запрашиваемая статья не существует!
                   </div>
                 </div>
-          <?php
-        } else
-        {
+              </div>
+            </section>
+            <!-- Секция сайдбара -->
+            <section class="content__right col-md-4">
+              <?php include "includes/sidebar.php";
+              ?>
+            </section>
+          </div>
+        </div>
+      </div>
+    <?php
+    } else {
       $art = mysqli_fetch_assoc($article);
       mysqli_query($connection, "UPDATE `articles` SET `views` = `views` + 1 WHERE `id` =" . (int) $art['id']);
-      ?>
+    ?>
       <div id="content">
-      <div class="container">
-        <div class="row">
+        <div class="container">
+          <div class="row">
 
-          <section class="content__left col-md-8">
-            <div class="block">
-              <a><?php echo $art['views'] ?> просмотров</a>
-              <h3><?php echo $art['title'];?></h3>
-              <div class="block__content">
-                <img src="/media/images/<?php echo $art['image']?>">
-                <div class="full-text">
-                <?php  echo $art['text']?>
+            <section class="content__left col-md-8">
+              <div class="block">
+                <a><?php echo $art['views'] ?> просмотров</a>
+                <h3><?php echo $art['title']; ?></h3>
+                <div class="block__content">
+                  <img src="/media/images/<?php echo $art['image'] ?>">
+                  <div class="full-text">
+                    <?php echo $art['text'] ?>
+                  </div>
                 </div>
-              </div>
               </div>
 
               <!-- Комментарии -->
               <?php include "includes/comments_block.php" ?>
-            </div>
+          </div>
 
-            <?php include "includes/comment_add_form.php" ?>
+          <?php include "includes/comment_add_form.php" ?>
           </section>
           <!-- Секция сайдбара -->
           <section class="content__right col-md-4">
@@ -72,12 +70,13 @@
           </section>
         </div>
       </div>
-    </div>
-
-    <?php
-    }
-  ?>
-    <?php include "includes/footer.php";?>
   </div>
+
+<?php
+    }
+?>
+<?php include "includes/footer.php"; ?>
+</div>
 </body>
+
 </html>
